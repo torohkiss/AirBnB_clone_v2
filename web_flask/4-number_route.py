818a130805daace808f42ea9c0de0719e81f4772
torohkiss <torohkiss@yahoo.com>
2024-07-28 16:06:13 -0700

    task 4

--- web_flask/4-number_route.py
+++ web_flask/4-number_route.py
@@ -31,11 +31,10 @@ def pythoniscool(text="is cool"):
     return 'Python ' + text.replace('_', ' ')
 
 
-@app.route("/number/<n>", strict_slashes=False)
+@app.route("/number/<int:n>", strict_slashes=False)
 def isnum(n):
     """ display “n is a number” if n is an integer"""
-    if isinstance(n, int):
-        return " n is a number"
+    return f"{n} is a number"
 
 
 if __name__ == '__main__':
