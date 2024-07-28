086e497d7bcf9c314046d1dceb69b1a73a9a757e
torohkiss <torohkiss@yahoo.com>
2024-07-28 16:02:50 -0700

    task 4

---
+++ web_flask/4-number_route.py
@@ -0,0 +1,42 @@
+#!/usr/bin/python3
+"""script that starts a Flask web application"""
+
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def helloflask():
+    """Returns hello hbnb"""
+    return "Hello HBNB!"
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    """returns hbnb"""
+    return "HBNB"
+
+
+@app.route("/c/<text>", strict_slashes=False)
+def cisfun(text):
+    """displays C followed by a formated text value"""
+    return 'C ' + text.replace('_', ' ')
+
+
+@app.route('/python/', strict_slashes=False)
+@app.route("/python/<text>", strict_slashes=False)
+def pythoniscool(text="is cool"):
+    """display “Python ”, followed by the text value"""
+    return 'Python ' + text.replace('_', ' ')
+
+
+@app.route("/number/<n>", strict_slashes=False)
+def isnum(n):
+    """ display “n is a number” if n is an integer"""
+    if isinstance(n, int):
+        return " n is a number"
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
