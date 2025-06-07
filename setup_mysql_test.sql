af72d9da6172b11238229301ec395326aad88768
torohkiss <torohkiss@yahoo.com>
2025-06-07 19:51:46 +0100

    task setup mysql testt

--- setup_mysql_test.sql
+++ setup_mysql_test.sql
@@ -7,7 +7,7 @@
 -- hbnb_test should have SELECT privilege on the database performance_schema (and only this database)
 -- If the database hbnb_test_db or the user hbnb_test already exists, your script should not fail
 
-CREATE DAYABASE IF NOT EXISTS hbnb_test_db;
+CREATE DATABASE IF NOT EXISTS hbnb_test_db;
 CREATE USER 'hbnb_test'@'localhost' IDENTIFIED BY 'hbnb_test_pwd';
 GRANT PRIVILEGE ON hbnb_test_db.* TO 'hbnb_test'@'localhost';
 GRANT SELECT ON performance_schema.* TO 'hbnb_test'@'localhost';
