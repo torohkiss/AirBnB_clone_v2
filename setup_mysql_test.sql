b0cc973daf2698f61870286bad59c070e7b4564a
torohkiss <torohkiss@yahoo.com>
2025-06-08 04:50:30 +0100

    task setup mysql tests

--- setup_mysql_test.sql
+++ setup_mysql_test.sql
@@ -8,7 +8,7 @@
 -- If the database hbnb_test_db or the user hbnb_test already exists, your script should not fail
 
 CREATE DATABASE IF NOT EXISTS hbnb_test_db;
-CREATE USER 'hbnb_test'@'localhost' IDENTIFIED BY 'hbnb_test_pwd';
-GRANT PRIVILEGE ON hbnb_test_db.* TO 'hbnb_test'@'localhost';
+CREATE USER IF NOT EXISTS 'hbnb_test'@'localhost' IDENTIFIED BY 'hbnb_test_pwd';
+GRANT ALL PRIVILEGES ON hbnb_test_db.* TO 'hbnb_test'@'localhost';
 GRANT SELECT ON performance_schema.* TO 'hbnb_test'@'localhost';
 FLUSH PRIVILEGES;
